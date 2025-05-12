950feacbe1b98754f730fa437c9299ab75dd6897
Nicolas Agustin Muñoz <nam.2013.oct@gmail.com>
2025-05-12 18:03:28 -0300

    Inicio

---
+++ src/web/__init__.py
@@ -0,0 +1,10 @@
+from flask import Flask
+from flask import render_template
+def create_app(env="development", static_folder="../static", template_folder="../templates"):
+    app = Flask(__name__)
+    
+    @app.route('/')
+    def home():
+        return render_template("home.html")
+    
+    return app
