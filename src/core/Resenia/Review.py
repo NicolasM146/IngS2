b7482a3ffd45e48c78831990accaf76dd3728dfe
Juansin <juancruzpissaco@gmail.com>
2025-06-25 17:38:49 -0300

    Corrijo bug

--- src/core/Resenia/Review.py
+++ src/core/Resenia/Review.py
@@ -4,7 +4,7 @@ class Review(db.Model):
     __tablename__ = 'reviews'
 
     id = db.Column(db.Integer, primary_key=True)
-    text = db.Column(db.String(255), nullable=False)
+    comment = db.Column(db.String(255), nullable=False)
 
     user_id = db.Column(db.Integer, db.ForeignKey('users.id'), nullable=False)
     user = db.relationship("User", back_populates="reviews")
