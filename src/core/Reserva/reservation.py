7e8146a0433b137ce580fa6bc83985d3ae35d7be
Juansin <juancruzpissaco@gmail.com>
2025-06-25 23:05:52 -0300

    Mas comentarios

--- src/core/Reserva/reservation.py
+++ src/core/Reserva/reservation.py
@@ -16,7 +16,7 @@ class Reservation(db.Model):
     end_date = db.Column(db.Date, nullable=False)
     price_per_night = db.Column(db.Float, nullable=False)
     created_at = db.Column(db.DateTime, default=datetime.utcnow)
-    status = db.Column(db.String(20), default='pending')
+    status = db.Column(db.String(20), default='pending') # pending, current, done
 
     rental_id = db.Column(db.Integer, db.ForeignKey('rentals.id'), nullable=False)
     rental = db.relationship("Rental", back_populates="reservations")
